19c6aa7160b1c6eed104edd8859a5e5afc718919
Henrique Lopes <riquellopes@gmail.com>
2016-02-04 23:22:24 -0200

    creating the basic files

---
+++ adsense.py
@@ -0,0 +1,5 @@
+ADSENSE_HEAD_TPL = """
+    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
+                 data-ad-client="{0}"
+                data-ad-slot="{1}"></ins>
+""".strip()
