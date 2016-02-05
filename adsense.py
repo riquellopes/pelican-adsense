3375eb431212a31ab27294abf91ca0fa84d7c86b
Henrique Lopes <riquellopes@gmail.com>
2016-02-05 00:41:20 -0200

    basic implementation

--- adsense.py
+++ adsense.py
@@ -1,5 +1,42 @@
-ADSENSE_HEAD_TPL = """
-    <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
-                 data-ad-client="{0}"
-                data-ad-slot="{1}"></ins>
-""".strip()
+from __future__ import unicode_literals
+
+import re
+from pelican import signals
+from jinja2 import Template
+
+ADSENSE_REGEX = re.compile(
+    r'(<p>\[adsense:client\=([^\]]+),slot\=([0-9]+),type_ad\=([a-zA-Z]+)?\]</p>)')
+
+
+class Tpl(object):
+    HEAD = """
+        <ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px"
+                     data-ad-client="{{client}}"
+                    data-ad-slot="{{slot}}"></ins>
+    """.strip()
+
+
+def get_tpl(type_ad):
+    return getattr(Tpl, type_ad.upper())
+
+
+def add_adsense_tag(generator):
+    """
+        [adsense:client=ca-pub-123456,slot=123456,type_ad=head]
+    """
+
+    for article in generator.articles:
+        for adsense in ADSENSE_REGEX.findall(article._content):
+            tpl = Template(get_tpl(adsense[3]))
+            context = generator.context.copy()
+            context.update({
+                'client': adsense[1],
+                'slot': adsense[2],
+            })
+
+            replacement = tpl.render(context)
+            article._content = article._content.replace(adsense[0], replacement)
+
+
+def register():
+    signals.article_generator_finalized.connect(add_adsense_tag)
---
+++ test_adsense.py
@@ -0,0 +1,27 @@
+import pytest
+from adsense import ADSENSE_REGEX, get_tpl, Tpl
+
+
+def test_get_head_adsense():
+    """
+        <ins class="adsbygoogle"
+            style="display:inline-block;width:728px;height:90px"
+            data-ad-client="pub-123456"
+            data-ad-slot="123456"></ins>
+    """
+    match = ADSENSE_REGEX.findall(
+        "<p>[adsense:client=ca-pub-123456,slot=123456,type_ad=head]</p>")[0]
+
+    assert match[1] == "ca-pub-123456"
+    assert match[2] == "123456"
+    assert match[3] == "head"
+
+
+def test_get_tpl_head():
+    tpl = get_tpl("head")
+    assert tpl == Tpl.HEAD
+
+
+def test_does_not_tpl_exists():
+    with pytest.raises(AttributeError):
+        get_tpl("anything")
