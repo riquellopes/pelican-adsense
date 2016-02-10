ca4831bdd03640fa5b3583a4325debfc03d7a803
Henrique Lopes <riquellopes@gmail.com>
2016-02-10 15:58:18 -0200

    add a new test

--- test_adsense.py
+++ test_adsense.py
@@ -3,18 +3,19 @@ from adsense import ADSENSE_REGEX, get_tpl, Tpl
 
 
 def test_get_head_adsense():
-    """
-        <ins class="adsbygoogle"
-            style="display:inline-block;width:728px;height:90px"
-            data-ad-client="pub-123456"
-            data-ad-slot="123456"></ins>
-    """
     match = ADSENSE_REGEX.findall(
         "<p>[adsense:client=ca-pub-123456,slot=123456,type_ad=head]</p>")[0]
 
     assert match[1] == "ca-pub-123456"
     assert match[2] == "123456"
     assert match[3] == "head"
+
+
+def test_get_adsense_error():
+    match = ADSENSE_REGEX.findall(
+        "<p>[adsense:client=ca-pub-123456,slot=123456]</p>")
+
+    assert len(match) == 0
 
 
 def test_get_tpl_head():
